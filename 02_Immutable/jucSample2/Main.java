cf2d40523c98c1347bb7068e327b87145b4e5a98
ytgw <tagawa0920@gmail.com>
2025-10-17 15:44:27 +0000

    2章のjucSample2アプリを追加。

---
+++ 02_Immutable/jucSample2/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final List<Integer> list = Collections.synchronizedList(new ArrayList<Integer>());
+        new WriterThread(list).start();
+        new ReaderThread(list).start();
+    }
+}
