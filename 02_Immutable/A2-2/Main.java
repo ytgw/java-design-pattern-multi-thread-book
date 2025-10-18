e99eff7dfa287762b1f4c772fc9f236d505516f4
ytgw <tagawa0920@gmail.com>
2025-10-18 06:05:25 +0000

    練習問題2の解答例を追加。

---
+++ 02_Immutable/A2-2/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "BAT";
+        String t = s.replace('B', 'C');
+        System.out.println("s = " + s);
+        System.out.println("t = " + t);
+
+        if (s == t) {
+            System.out.println("s == t");
+        } else {
+            System.out.println("s != t");
+        }
+    }
+}
