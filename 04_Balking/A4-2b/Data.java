ddca762ce70d6ee05f324ca23d7d1167bef2cdd3
ytgw <tagawa0920@gmail.com>
2025-10-19 08:39:36 +0000

    4章の練習問題2の解答例を追加。

---
+++ 04_Balking/A4-2b/Data.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.Writer;
+
+
+public class Data {
+    private final String fileName;  // 保存するファイルの名前
+    private String content;  // データの内容
+    private boolean changed;  // 変更した内容が葉ぞんされていないならtrue
+
+    public Data(String fileName, String content) {
+        this.fileName = fileName;
+        this.content = content;
+        this.changed = true;
+    }
+
+    // データの内容を書き換える
+    public synchronized void change(String newContent) {
+        this.content = newContent;
+        this.changed = true;
+    }
+
+    // データの内容が変更されていたらファイルに保存する
+    public void save() throws IOException {  // not synchronized
+        if (!changed) {
+            System.out.println(Thread.currentThread().getName() + " balks");
+            return;
+        }
+        doSave();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        changed = false;
+    }
+
+    // データの内容を実際にファイルに保存する
+    private void doSave() throws IOException {
+        System.out.println(Thread.currentThread().getName() + " calls doSave, content = " + content);
+        Writer writer = new FileWriter(fileName);
+        writer.write(content);
+        writer.close();
+    }
+}
