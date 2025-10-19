3b4aeaa36a1d1e5c9ca7b33285421cd1c9d50524
ytgw <tagawa0920@gmail.com>
2025-10-19 16:57:08 +0000

    5章の練習問題8の解答例を追加。

---
+++ 05_ProducerConsumer/A5-8/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Table table = new Table(3);  // ケーキを3個までおけるテーブルを作る
+
+        new MakerThread("Maker-1", table, 31415).start();
+        new MakerThread("Maker-2", table, 92653).start();
+        new MakerThread("Maker-3", table, 58979).start();
+
+        new EaterThread("Eater-1", table, 32384).start();
+        new EaterThread("Eater-2", table, 62643).start();
+        new EaterThread("Eater-3", table, 38327).start();
+
+        new LazyThread("Lazy-1", table).start();
+        new LazyThread("Lazy-2", table).start();
+        new LazyThread("Lazy-3", table).start();
+        new LazyThread("Lazy-4", table).start();
+        new LazyThread("Lazy-5", table).start();
+        new LazyThread("Lazy-6", table).start();
+        new LazyThread("Lazy-7", table).start();
+    }
+}
