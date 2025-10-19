f25ed1e19151f40db0e9196de0cdb487eaba40c3
ytgw <tagawa0920@gmail.com>
2025-10-19 06:34:42 +0000

    4章のTimeoutアプリを追加。

---
+++ 04_Balking/Timeout/Host.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeoutException;
+
+
+public class Host {
+    private final long timeout;  // タイムアウト値
+    private boolean ready = false;  // メソッドを実行して良いならtrue
+
+    public Host(long timeout) {
+        this.timeout = timeout;
+    }
+
+    // 状態の変更をする
+    public synchronized void setExecutable(boolean on) {
+        ready = on;
+        notifyAll();
+    }
+
+    // 状態を考えたうえで実行する
+    public synchronized void execute() throws InterruptedException, TimeoutException {
+        long start = System.currentTimeMillis();  // 開始時刻
+        while (!ready) {
+            long now = System.currentTimeMillis();  // 現在時刻
+            long rest = timeout - (now - start);  // 残りの待ち時間
+            if (rest <= 0) {
+                throw new TimeoutException("now - start = " + (now - start) + ", timeout = " + timeout);
+            }
+            wait(timeout);
+        }
+        doExecute();
+    }
+
+    // 実際の処理
+    private void doExecute() {
+        System.out.println(Thread.currentThread().getName() + " calls doExecute");
+    }
+}
---
+++ 04_Balking/Timeout/Main.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.TimeoutException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Host host = new Host(10_000);
+        try {
+            System.out.println("execute BEGIN");
+            host.execute();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
