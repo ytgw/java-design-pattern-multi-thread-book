e4a4274e2a2380c309d984f1174f22d56913328c
ytgw <tagawa0920@gmail.com>
2025-10-22 09:14:58 +0000

    6章の練習問題3の解答例を追加。

---
+++ 06_ReadeWriteLock/A6-3a/WriterThread.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+
+public class WriterThread extends Thread {
+    private static final Random random = new Random();  // staticで大丈夫か?
+    private final Data data;
+    private final String filler;
+    private int index = 0;
+
+    public WriterThread(Data data, String filler) {
+        this.data = data;
+        this.filler = filler;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                char c = nextChar();
+                data.write(c);
+                Thread.sleep(random.nextInt(3_000));
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    private char nextChar() {
+        char c = filler.charAt(index);
+        index++;
+        if (index >= filler.length()) {
+            index = 0;
+        }
+        return c;
+    }
+}
