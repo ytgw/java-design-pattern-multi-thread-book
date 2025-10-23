d3b1403df19c32f5c6475e56fa96ec34a2034b00
ytgw <tagawa0920@gmail.com>
2025-10-23 14:38:20 +0000

    7章の練習問題6のアプリを追加。

---
+++ 07_ThreadPerMessage/Practice06/MiniServer.java
@@ -0,0 +1,37 @@
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+
+
+public class MiniServer {
+    private final int portNumber;
+
+    public MiniServer(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    public void execute() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        System.out.println("Listening on " + serverSocket);
+        try {
+            while (true) {
+                System.out.println("Accepting...");
+                Socket cilentSocket = serverSocket.accept();
+                System.out.println("Connected to " + cilentSocket);
+                new Thread() {
+                    public void run() {
+                        try {
+                            Service.service(cilentSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
