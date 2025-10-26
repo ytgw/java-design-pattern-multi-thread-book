b7c5f9c09555e3163cbee37d187d38cea5e7561c
ytgw <tagawa0920@gmail.com>
2025-10-26 15:03:07 +0000

    8章のjucSampleアプリを追加。

---
+++ 08_WorkerThread/jucSample/Main.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        try {
+            new ClientThread("Alice", executorService).start();
+            new ClientThread("Bobby", executorService).start();
+            new ClientThread("Chris", executorService).start();
+
+            // 約5秒待つ
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+        } finally {
+            executorService.shutdown();
+        }
+    }
+}
