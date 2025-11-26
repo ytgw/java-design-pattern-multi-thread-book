5de1c825963d93ebf829be0023a1206f24a28807
ytgw <tagawa0920@gmail.com>
2025-11-26 09:06:57 +0000

    9章のjucSampleアプリを追加。

---
+++ 09_Future/jucSample/Host.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.Callable;
+
+
+public class Host {
+    public Data request(final int count, final char c) {
+        System.out.println("    request(" + count + ", " + c + ") BEGIN");
+
+        // (1) FutureDataのインスタンスを作る
+        //     (コンストラクタにCallable<RealData>を渡す)
+        FutureData future = new FutureData(
+            new Callable<RealData>() {
+                public RealData call() {
+                    return new RealData(count, c);
+                }
+            }
+        );
+
+        // (2) RealDataのインスタンスを作るための新しいスレッドを起動する
+        new Thread(future).start();
+
+        System.out.println("    request(" + count + ", " + c + ") END");
+
+        // (3) FutureDataのインスタンスを戻り値とする
+        return future;
+    }
+}
