3fb1a95330c93d9cdabd8a5972e9dc7365909d96
ytgw <tagawa0920@gmail.com>
2026-01-12 13:03:35 +0000

    11章の練習問題6の解答例を追加。

---
+++ 11_ThreadSpecificStorage/A11-6/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static final int TASKS = 10;
+
+    public static void main(String[] args) {
+        for (int t = 0; t < TASKS; t++) {
+            // ログに書き込むクラス
+            Runnable printTask = new Runnable() {
+                public void run() {
+                    Log.println("Hello!");
+                    Log.close();
+                }
+            };
+            // タスクの実行
+            new Thread(printTask).start();
+        }
+    }
+}
