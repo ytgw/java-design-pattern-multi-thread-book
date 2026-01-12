6c22c500223bf0dd66d6cf38775e6040a68bd4be
ytgw <tagawa0920@gmail.com>
2026-01-12 11:37:15 +0000

    11章のSample1アプリを追加。

---
+++ 11_ThreadSpecificStorage/Sample1/Log.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Log {
+    private static PrintWriter writer = null;
+
+    // writerフィールドの初期化
+    static {
+        try {
+            writer = new PrintWriter(new FileWriter("log.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // ログを書く
+    public static void println(String s) {
+        writer.println(s);
+    }
+
+    // ログを閉じる
+    public static void close() {
+        writer.println("=== End of log ===");
+        writer.close();
+    }
+}
